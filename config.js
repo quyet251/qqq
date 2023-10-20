fd53c84e164b0ec8bfee09142473ad10c2e88d3d
quyet251 <96272840+quyet251@users.noreply.github.com>
2023-10-20 14:02:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+
+const eventName = 'click vào đây nhé 2 chị :>';
+const titleCard = 'Gửi tặng Vũ Thảo và Huyền My';
+const contentCard = 'Có một ngày 20/10 thật vui vẻ nhé';
+
+const giftImage = 'hoa.jpg';
+// const base64 = '';
+// const giftImageBase64 = "data:image/png;base64, " + base64;
